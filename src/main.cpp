56074ffa36a90340004e2bc055104b209fefd7f2
Mikhail Buyanov <mi.buyanov@yandex.ru>
2025-08-10 16:14:49 +0300

    hello world

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Includes the input/output stream library
+
+int main() { // The main function, where program execution begins
+    std::cout << "Hello, World!" << std::endl; // Prints "Hello, World!" to the console
+    return 0; // Indicates successful program execution
+}
