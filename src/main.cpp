913ad1c470812fe12d3720371faa6243347a720d
Mikhail Buyanov <mi.buyanov@yandex.ru>
2025-08-10 17:33:32 +0300

    Added allegro5

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,73 @@
-#include <iostream> // Includes the input/output stream library
+#include <cstdio>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 
-int main() { // The main function, where program execution begins
-    std::cout << "Hello, World!" << std::endl; // Prints "Hello, World!" to the console
-    return 0; // Indicates successful program execution
+int main(int argc, char **argv) {
+    bool running = true;
+
+    // Initialize allegro
+    if (!al_init()) {
+        fprintf(stderr, "Failed to initialize allegro.\n");
+        return 1;
+    }
+
+    // Initialize allegro font addon
+    if (!al_init_font_addon()) {
+        fprintf(stderr, "Failed to initialize allegro font addon.\n");
+        return 1;
+    }
+
+    // Initialize allegro ttf addon
+    if (!al_init_ttf_addon()) {
+        fprintf(stderr, "Failed to initialize allegro ttf addon.\n");
+        return 1;
+    }
+
+    // Create the event queue
+    ALLEGRO_EVENT_QUEUE *event_queue = nullptr;
+    event_queue = al_create_event_queue();
+    if (!event_queue) {
+        fprintf(stderr, "Failed to create event queue.");
+        return 1;
+    }
+
+    al_install_keyboard();
+    al_install_mouse();
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    al_register_event_source(event_queue, al_get_mouse_event_source());
+
+
+    ALLEGRO_DISPLAY *display = nullptr;
+    display = al_create_display(1000, 1000);
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+
+    // drawing hello world
+    ALLEGRO_FONT *debug_font = al_load_ttf_font("./resources/clacon2.ttf", 14, 0);
+    al_draw_text(debug_font, al_map_rgb(255, 255, 255), 10, 10, 0, "Hello, World!");
+    al_flip_display();
+
+    while (running) {
+        ALLEGRO_EVENT event;
+        ALLEGRO_TIMEOUT timeout;
+        al_init_timeout(&timeout, 0.06);
+        bool get_event = al_wait_for_event_until(event_queue, &event, &timeout);
+
+        // Handle the event
+        if (get_event) {
+            switch (event.type) {
+                case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                    running = false;
+                    break;
+                default:
+                    fprintf(stderr, "Unsupported event received: %d\n", event.type);
+                    break;
+            }
+        }
+    }
+
+    al_destroy_display(display);
+    al_destroy_event_queue(event_queue);
+
+    return 0;
 }
